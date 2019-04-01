1d4ab446301d58489bea469d6434d859edc89f0b
Phil Davis <phil@jankaritech.com>
2019-04-01 21:11:42 +0545

    Adjust PHPUnit Framework namespace for PHPUnit6

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -12,7 +12,7 @@ require_once __DIR__.'/../../../../lib/base.php';
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
 \OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/', true);
 
-if (!\class_exists('PHPUnit_Framework_TestCase')) {
+if (!\class_exists('PHPUnit\Framework\TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 
