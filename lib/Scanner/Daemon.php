172c4198b5ec5cd2bcd1b5d0321b410a3eac6fb9
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2025-06-19 14:46:28 +0200

    fix: Check we're connection to a ClamAV service

--- lib/Scanner/Daemon.php
+++ lib/Scanner/Daemon.php
@@ -80,7 +80,43 @@ class Daemon extends External {
 				)
 			);
 		}
+
+		// Check we're connecting to a ClamAV daemon
+		$pingResult = $this->sendCommand("PING", 6); // PONG plus newline chars is expected
+		if (\rtrim($pingResult, "\r\n") !== "PONG") {
+			throw new InitException("Unexpected response to ping: $pingResult");
+		}
+
+		$versionResult = $this->sendCommand("VERSION", 500);
+		// The response can vary, and it's difficult to predict the size of the response,
+		// but 500 bytes should be more than enough to fit the whole response.
+		// Just check that it starts with "ClamAV"
+		if (\strpos($versionResult, 'ClamAV') !== 0) {
+			throw new InitException("Unexpected response to version: $versionResult");
+		}
+
 		// request scan from the daemon
 		@\fwrite($this->getWriteHandle(), "nINSTREAM\n");
+	}
+
+	/**
+	 * NOTE: ClamAV closes the connection after each command, so we need to
+	 * open a new connection each time.
+	 */
+	private function sendCommand(string $clamavCmd, int $maxResponseSize): string {
+		$handle = @\fsockopen($this->avHost, $this->avPort, $errorCode, $errorMsg);
+		if ($handle === false) {
+			throw new InitException("Failed to connect: {$errorMsg} ({$errorCode})");
+		}
+
+		$bytesWritten = @\fwrite($handle, "{$clamavCmd}\n");
+		if ($bytesWritten !== \strlen($clamavCmd) + 1) {
+			throw new InitException("Failed to write {$clamavCmd} command in the handle");
+		}
+
+		$result = @\fgets($handle, $maxResponseSize);
+		@fclose($handle);
+
+		return $result;
 	}
 }
